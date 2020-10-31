14ba914d1af6ad349370df03c72373513b1764fa
Shamilprog <shamilprog@yandex.ru>
2020-10-31 22:36:36 +0300

    Click on a list item now opens detail flow.

--- app/src/main/java/ru/shamilprog/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ru/shamilprog/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package ru.shamilprog.android.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -46,9 +47,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(),
-                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
 
