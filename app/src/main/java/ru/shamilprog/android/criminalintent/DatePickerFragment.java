45e479c9f02291279656dd58594ff68480ca3ad6
Shamilprog <shamilprog@yandex.ru>
2020-11-02 23:26:38 +0300

    Create a fragment for a date picker dialog.

---
+++ app/src/main/java/ru/shamilprog/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,21 @@
+package ru.shamilprog.android.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+public class DatePickerFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+
+}
