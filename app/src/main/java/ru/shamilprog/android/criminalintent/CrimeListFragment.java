e2e6fbd8b120bb5c916fcebbc457db0254dd8821
Shamilprog <shamilprog@yandex.ru>
2020-11-11 00:31:05 +0300

    Now user can see database changes in CrimeListFragment.

--- app/src/main/java/ru/shamilprog/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ru/shamilprog/android/criminalintent/CrimeListFragment.java
@@ -84,6 +84,10 @@ public class CrimeListFragment extends Fragment {
         public int getItemCount() {
             return mCrimes.size();
         }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
     }
 
     @Override
@@ -174,6 +178,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
